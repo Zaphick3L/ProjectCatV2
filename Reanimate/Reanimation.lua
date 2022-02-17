65ec664fd8b012527895ec85fcf49794d1b3dddd
Zaphick3L <99723503+Zaphick3L@users.noreply.github.com>
2022-02-17 18:01:37 +0100

    Update Reanimation.lua

--- Reanimate/Reanimation.lua
+++ Reanimate/Reanimation.lua
@@ -1,31 +1,32 @@
 --[[
-Updates:
-
-1.2.3 - Fixed Camera.
-1.1.2 - Added Anti Tool, Changed default align values.
-1.1.1 - Credits time 2 > 1.5, Removed Useless global.
-1.1.0 - Added Credits + Custom Velocity, Bugfxi with bullet + Jump support
-1.0.0 - Released.
-
+-----------------//// Updates:
+1.3:
+[ + ] New Animate Script for Animations.
+[ - ] Removed ProductionTakeOne from credits (I was using his Animate back then.)
+[ * ] Fling Force increased (3500 > 4000)
+[ + ] Made the UpdateLog more Readable.
+[ * ] Updated Credits, added reanimation version as well.
+[ * ] BugFix with some strings.
+[ * ] Changed default align values. (MaxForce/MaxTorque (700000000 > 9e9))
+1.2.3:
+[ * ] Fixed Camera.
+1.1.2:
+[ + ] Added Anti Tool to avoid breaking on r15.
+[ * ] Changed default align values. (MaxForce/MaxTorque (76007600 > 700000000))
+1.1.1:
+[ * ] Credits time 2 > 1.5
+[ - ] Removed Useless global.
+1.1.0:
+[ + ] Added Credits.
+[ + ] Custom Velocity.
+[ * ] Bugfix with bullet .
+[ + ] Jump support.
+1.0.0:
+[ + ] Released.
 
 
 Credits:
 Gelatek (Everything)
-ProductionTakeOne (R15 To R6 Animations)
-
-Main:
-Align: Done
-R15: Done
-R6: Done
-
-Properties:
-Fling: Done
-Bullet: Done
-RigEnabled: Done
-Net: Done
-Physics: Done
-Remove Anims: Done
-NoNetless: Done
 ]]--
 if not _G.Velocity then _G.Velocity = Vector3.new(0,-36.5,0) end
 if not _G.Fling then _G.Fling = false end
@@ -41,7 +42,7 @@ function Attach(P0,P1,Position,Orientation)
 	end
    local AP = Instance.new("AlignPosition")
    AP.Parent = P1
-   AP.MaxForce = 700000000
+   AP.MaxForce = 9e9
    AP.Responsiveness = 200
    AP.MaxVelocity = math.huge
    AP.ApplyAtCenterOfMass = false
@@ -51,11 +52,11 @@ function Attach(P0,P1,Position,Orientation)
    
    local AO = Instance.new("AlignOrientation")
    AO.Parent = P0
-   AO.MaxTorque = 700000000
+   AO.MaxTorque = 9e9
    AO.Responsiveness = 200
    AO.MaxAngularVelocity = math.huge
    AO.RigidityEnabled = false
-   AO.Name = "Position"
+   AO.Name = "Orientation"
    
     if _G.RigEnabled == true then
     	 AP.RigidityEnabled = true
@@ -79,7 +80,7 @@ end
 
 local Character = game.Players.LocalPlayer.Character; Character.Archivable = true
 local Clone = game:GetObjects('rbxassetid://8440552086')[1]
-Clone.Parent = Character; Clone.Name = "Dummy"
+Clone.Parent = Character; Clone.Name = "GelatekReanimate"
 for _, v in pairs(Clone:GetDescendants()) do if v:IsA("BasePart") or v:IsA("Decal") then
 	v.Transparency = 1
 end end
@@ -109,11 +110,11 @@ function Netless()
 				v.Velocity = _G.Velocity or Vector3.new(25.05,25.05,25.05)
 			elseif _G.NoNetless == false and _G.Fling == true then
 				v.Velocity = _G.Velocity or Vector3.new(25.05,25.05,25.05)
-				Torso.RotVelocity = Vector3.new(3500,3500,3500)
+				Torso.RotVelocity = Vector3.new(4000,4000,4000)
 			elseif _G.NoNetless == true and _G.Fling == false then
 				
 			elseif _G.NoNetless == true and _G.Fling == true then
-				Torso.RotVelocity = Vector3.new(3500,3500,3500)
+				Torso.RotVelocity = Vector3.new(4000,4000,4000)
 			end
 		end	
 	end
@@ -127,6 +128,9 @@ function Netless()
 			
 		end
 	end)
+		for _,v in pairs(Humanoid:GetPlayingAnimationTracks()) do
+		v:Stop()
+	end
 end
 TriggerNetless = game:GetService("RunService").Heartbeat:Connect(Netless)
 game:GetService("UserInputService").JumpRequest:Connect(function()
@@ -143,6 +147,10 @@ end)
 Character.ChildAdded:Connect(function(tool)
     if tool:IsA("Tool") then tool:Destroy() end
 end)
+
+if workspace:FindFirstChildOfClass("Camera") then
+	workspace.Camera.CameraSubject = CloneHumanoid
+end
 
 if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 	Character.HumanoidRootPart:Destroy()
@@ -167,6 +175,7 @@ else
     for _,v in pairs(Humanoid:GetChildren()) do
         if v:IsA("NumberValue") then v:Destroy() end
     end
+        Character.Animate.Disabled = true
     Attach(Character.UpperTorso, Clone['Torso'],Vector3.new(0, 0.15, 0))
     Attach(Character.LowerTorso, Clone['Torso'],Vector3.new(0, 0.75, 0))
     Attach(Character.LeftUpperArm, Clone['Left Arm'],Vector3.new(0, -0.405, 0)) 
@@ -185,13 +194,12 @@ else
 		Humanoid:ChangeState("Physics")
 		Character["LowerTorso"].Name = "Bullet"
 	end
-    Character.Animate.Disabled = true
 end
-if workspace:FindFirstChildOfClass("Camera") then
-	workspace.Camera.CameraSubject = CloneHumanoid
-end
+
 local Credits = Instance.new("Message", workspace)
-Credits.Text = "Reanimation by Gelatek! R15-To-R6 Animate by ProductionTakeOne!"
+Credits.Text = "Reanimation by Gelatek! Current Version: 1.3"
 wait(1.5)
 Credits:Destroy()
-
+if _G.RemoveAnims == false then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Zaphick3L/ProjectCatV2/main/Utilities/R15ToR6Animate.lua"))()
+end
